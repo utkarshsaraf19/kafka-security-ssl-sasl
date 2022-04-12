f57ab939ef1b3753c4bc634fad2f5cefb6c04570
usaraf <utkarsh.saraf@qio.io>
2022-04-12 23:06:25 +0530

    changes

--- sasl-scram/zookeeper_scripts.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker exec -it zookeeper-1 echo stat | nc localhost 22181 | grep Mode
-docker exec -it zookeeper-2 echo stat | nc localhost 32181 | grep Mode
-docker exec -it zookeeper-3 echo stat | nc localhost 42181 | grep Mode
